73dc62d36fa837216a855b2a5866b15c33036556
lijunjie2232 <2533584225@qq.com>
2026-02-16 18:21:14 +0900

    fix init

--- backend/faceapi/db/init_account.py
+++ backend/faceapi/db/init_account.py
@@ -1,11 +1,12 @@
-from ..models import UserModel
-from ..utils import hash_password
 from ..core import _CONFIG_
+from ..models import UserModel
+from ..utils.pass_utils import hash_password
+
 
 async def create_init_account():
     """
     Create the init account
-    
+
     default init account:
         ADMIN_USERNAME: str = os.getenv("ADMIN_USERNAME", "admin")
         ADMIN_PASSWORD: str = os.getenv("ADMIN_PASSWORD", "admin")
@@ -17,11 +18,14 @@ async def create_init_account():
 
     created_user = await UserModel.get_or_create(
         username=_CONFIG_.ADMIN_USERNAME,
-        email=_CONFIG_.ADMIN_EMAIL,
-        full_name=_CONFIG_.ADMIN_FULL_NAME,
-        hashed_password=hashed_password,
-        is_active=True,
-        is_admin=True,
+        defaults={
+            "username": _CONFIG_.ADMIN_USERNAME,
+            "email": _CONFIG_.ADMIN_EMAIL,
+            "full_name": _CONFIG_.ADMIN_FULL_NAME,
+            "hashed_password": hashed_password,
+            "is_active": True,
+            "is_admin": True,
+        },
     )
 
     assert created_user is not None, "Failed to create init account"
