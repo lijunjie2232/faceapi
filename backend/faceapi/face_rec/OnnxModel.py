64413b7f097acf632b2332d7ce14dd9b4f659941
lijunjie2232 <2533584225@qq.com>
2026-02-16 18:22:15 +0900

    add onnx loader

---
+++ backend/faceapi/face_rec/OnnxModel.py
@@ -0,0 +1,17 @@
+import onnxruntime as ort
+
+from .FaceRecModel import register_model
+
+@register_model("onnx")
+def load_onnx_model(weight, *args, **kwargs):
+    """
+    ONNXモデルをロードする
+
+    引数:
+        onnx_model_path: ONNXモデルファイルのパス
+
+    戻り値:
+        ONNXセッションオブジェクト
+    """
+    session = ort.InferenceSession(weight)
+    return session
