ac31b339a305a6fc563443869d3dfcae28c58a53
lijunjie2232 <2533584225@qq.com>
2026-03-02 15:25:04 +0900

    doc req

---
+++ backend/requirements_doc.py
@@ -0,0 +1,9 @@
+# Documentation dependencies
+mkdocs>=1.5.0
+mkdocs-i18n>=0.4.0
+mkdocs-material>=9.0.0
+mkdocs-static-i18n>=1.0.0
+mkdocs-git-revision-date-localized-plugin>=1.0.0
+mkdocs-minify-plugin>=0.7.0
+mkdocs-redirects>=1.2.0
+pymdown-extensions>=10.0
